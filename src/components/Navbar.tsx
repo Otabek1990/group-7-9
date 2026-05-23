6350edb7c6aefc2db2bded3e8120676422ebc9aa
Otabek1990 <fozilovotabek424@gmail.com>
2026-05-23 19:50:58 +0500

    first commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+
+export default function Navbar() {
+  return (
+    <nav>
+      <h2>Website nomi</h2>
+      <ul>
+        <li>Home</li>
+        <li>Aloqa</li>
+        <li>Kontakt</li>
+      </ul>
+    </nav>
+  )
+}
